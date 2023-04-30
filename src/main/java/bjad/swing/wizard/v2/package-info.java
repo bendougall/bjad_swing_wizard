5df4e132017e77f3c828f4a5280b807561703b40
Ben Dougall <ben.dougall@gmail.com>
2023-04-30 08:15:21 -0400

    2-v2-start-and-abstract-page-addition
    Starting version two of the wizard framework and adding the initial version of the Abstract Page class

---
+++ src/main/java/bjad/swing/wizard/v2/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Version 2 of the BJAD wizard framework, designed 
+ * to be more flexible for the developer to use in 
+ * their application.
+ *
+ * @author 
+ *   Ben Dougall
+ */
+package bjad.swing.wizard.v2;
